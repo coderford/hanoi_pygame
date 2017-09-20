dbe57e81d2284e4735f0951147636130e5e4d2da
Aditya Kumar <kumar.sava.aditya@gmail.com>
2017-09-20 19:40:39 +0530

    add menuscreen with difficulty select

--- __main__.py
+++ __main__.py
@@ -5,8 +5,12 @@ pygame.display.set_caption("Towers of Hanoi")
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
-done = False
+game_done = False
 framerate = 60
+
+# game vars:
+steps = 0
+plates = 3
 
 # colors:
 white = (255, 255, 255)
@@ -15,7 +19,43 @@ red = (255, 0, 0)
 gold = (239, 229, 51)
 blue = (78,162,196)
 
-while not done:
+def blit_text(screen, text, midtop, aa=True, font=None, font_name = None, size = None, color=(255,0,0)):
+    if font is None:                                    # font option is provided to save memory if font is
+        font = pygame.font.SysFont(font_name, size)     # already loaded and needs to be reused many times
+    font_surface = font.render(text, aa, color)
+    font_rect = font_surface.get_rect()
+    font_rect.midtop = midtop
+    screen.blit(font_surface, font_rect)
+
+def menu_screen():  # to be called before starting actual game loop
+    global screen, plates, game_done
+    menu_done = False
+    while not menu_done:  # every screen/scene/level has its own loop
+        screen.fill(white)
+        blit_text(screen, 'Towers of Hanoi', (320,100), font_name='sans serif', size=90, color=gold)
+        blit_text(screen, 'Use arrow keys to select difficulty:', (320, 220), font_name='sans serif', size=30, color=black)
+        blit_text(screen, str(plates), (320, 260), font_name='sans serif', size=40, color=blue)
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    menu_done = True
+                if event.key in [pygame.K_RIGHT, pygame.K_UP]:
+                    plates += 1
+                    if plates > 10:
+                        plates = 10
+                if event.key in [pygame.K_LEFT, pygame.K_DOWN]:
+                    plates -= 1
+                    if plates < 1:
+                        plates = 1
+            if event.type == pygame.QUIT:
+                menu_done = True
+                game_done = True
+        pygame.display.flip()
+        clock.tick(60)
+
+menu_screen()
+# main game loop:
+while not game_done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
